dffb77b0a62e2f3fce2afa7ba29dca1a9de2e218
Diwakar Jaiswal <38224086+diwakarjaiswal880@users.noreply.github.com>
2020-11-23 20:04:02 +0530

    Update ping_pong.go

--- ping_pong.go
+++ ping_pong.go
@@ -1,18 +1,42 @@
-// go concurrency
 package main
 
 import (
-	"fmt"
-	"time"
+    "fmt"
+    "time"
 )
 
-func main() {
-	go pingpong("Ping")
-	pingpong("pong")
+// The pinger prints a ping and waits for a pong
+func pinger(pinger <-chan int, ponger chan<- int) {
+    for {
+        <-pinger
+        fmt.Println("ping")
+        time.Sleep(time.Second)
+        ponger <- 1
+    }
 }
-func pingpong(t string) {
-	for i := 1; i < 20; i++ {
-		fmt.Println(t)
-		time.Sleep(time.Millisecond * 500)
-	}
+
+// The ponger prints a pong and waits for a ping
+func ponger(pinger chan<- int, ponger <-chan int) {
+    for {
+        <-ponger
+        fmt.Println("pong")
+        time.Sleep(time.Second)
+        pinger <- 1
+    }
+}
+
+func main() {
+    ping := make(chan int)
+    pong := make(chan int)
+
+    go pinger(ping, pong)
+    go ponger(ping, pong)
+
+    // The main goroutine starts the ping/pong by sending into the ping channel
+    ping <- 1
+
+    for {
+        // Block the main thread until an interrupt
+        time.Sleep(time.Second)
+    }
 }
